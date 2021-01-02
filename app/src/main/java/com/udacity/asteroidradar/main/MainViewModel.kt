861d41cf53fbfad15f367f987fcf445885ccbd67
Wallace-GS <wgsantos29@gmail.com>
2021-01-02 10:26:09 -0800

    parses json result

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.api.NasaApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,8 +25,9 @@ class MainViewModel : ViewModel() {
     private fun getAsteroidsInfo() {
         NasaApi.retrofitService.getAsteroids(Constants.API_KEY).enqueue(object: Callback<String> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body().toString()
-                Log.i("MainVM", response.body().toString())
+                val asteroidList = parseAsteroidsJsonResult(JSONObject(response.body()!!))
+                _response.value = asteroidList.toString()
+                Log.i("MainVM", asteroidList.toString())
             }
 
             override fun onFailure(call: Call<String>, t: Throwable) {
